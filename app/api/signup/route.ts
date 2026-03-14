d36b011009488d0a8c3b7c21db3072009df7e2ca
victor akubo <victorugbede89@gmail.com>
2026-03-14 19:50:50 +0100

    user route

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,23 +1,26 @@
-import {NextResponse} from "next/server"
+import { NextResponse } from "next/server"
 import AuthSchema from "../../../models/Users"
 
-let userauth = []
+let userauth: any[] = []
 
-export async function POST(req){
- try{
+export async function POST(req: Request) {
+  try {
     const body = await req.json()
-  const {email,name,password} = body; 
-  const createdUser = await AuthSchema.create({name,email,password})
-  userauth.push({name,email,password})
-  return NextResponse.json({
-    status:201,
-    message : "user created successfully",
-    user:createdUser,
-    count:userauth.length
-  })
- }catch(error){
-   return NextResponse.json({
-     message:error.message
-   })
- }
+    const { email, name, password } = body
+
+    const createdUser = await AuthSchema.create({ name, email, password })
+
+    userauth.push({ name, email, password })
+
+    return NextResponse.json({
+      status: 201,
+      message: "user created successfully",
+      user: createdUser,
+      count: userauth.length
+    })
+  } catch (error: any) {
+    return NextResponse.json({
+      message: error.message
+    })
+  }
 }
